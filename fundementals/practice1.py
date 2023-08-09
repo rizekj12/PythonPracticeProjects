fadd6898918fa55292c24835997feeffce53ffbc
Joshua Rizek <joshuarizek@gmail.com>
2023-08-08 21:58:43 -0400

    Update practice1.py

---
+++ fundementals/practice1.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+numStr = str(two_digit_number)
+
+numOne = int(numStr[0])
+
+numTwo = int(numStr[1])
+
+sum = numOne + numTwo
+print(sum)
