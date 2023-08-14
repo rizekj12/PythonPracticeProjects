b3c8c28a3ea3a9bbe6f89020892b59d9bcdc637f
Joshua Rizek <joshuarizek@gmail.com>
2023-08-14 11:10:24 -0400

    Update practice1.py

--- fundementals/practice1.py
+++ fundementals/practice1.py
@@ -22,3 +22,20 @@ if number % 2 == 0:
     print("This is an even number.")
 else :
     print("This is an odd number.")
+
+##rollercoster ride 
+
+print("welcome to the rollercoaster")
+height = int(input("what is your height in cm?"))
+
+if height >= 120:
+  print("you can ride the rollercoaster!")
+  age = int(input("What is your age?"))
+  if age < 12:
+    print("Please pay $5.")
+  elif age <= 18:
+    print("please pay $7")  
+  else:
+    print("Please pay $12")
+else:
+  print("Sorry, you have to grow taller before you can ride")
