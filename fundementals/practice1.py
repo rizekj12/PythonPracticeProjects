eb9bf5af501ca013550c9cab3554979569a9c4d7
Joshua Rizek <joshuarizek@gmail.com>
2023-08-14 11:00:41 -0400

    simple conditionals

--- fundementals/practice1.py
+++ fundementals/practice1.py
@@ -13,3 +13,12 @@ numTwo = int(numStr[1])
 
 sum = numOne + numTwo
 print(sum)
+
+
+## simple conditionals
+number = int(input("what is your height in cm?"))
+
+if number % 2 == 0:
+    print("This is an even number.")
+else :
+    print("This is an odd number.")
