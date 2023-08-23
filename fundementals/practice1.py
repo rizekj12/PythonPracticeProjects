e50be3fe75819d5c4abe2b925f838a5f06cf29f1
Joshua Rizek <joshuarizek@gmail.com>
2023-08-23 16:18:58 -0400

    Update practice1.py

--- fundementals/practice1.py
+++ fundementals/practice1.py
@@ -69,3 +69,18 @@ else:
 
 items = ["can","shovel","spoon","notebook"]
 
+
+# Import the random module here
+import random
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+random_num = random.randint(0, len(names) - 1)
+
+print(names[random_num])
+
+
