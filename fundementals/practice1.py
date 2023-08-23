1e4271e674bb86dcdd3d583716f6b32f0b79e742
Joshua Rizek <joshuarizek@gmail.com>
2023-08-22 21:24:08 -0400

    added more practice code and files

--- fundementals/practice1.py
+++ fundementals/practice1.py
@@ -1,53 +1,71 @@
-# 🚨 Don't change the code below 👇
-two_digit_number = input("Type a two digit number: ")
-# 🚨 Don't change the code above 👆
 
-####################################
-#Write your code below this line 👇
+import random
+import practiceModule
 
-numStr = str(two_digit_number)
+# # 🚨 Don't change the code below 👇
+# two_digit_number = input("Type a two digit number: ")
+# # 🚨 Don't change the code above 👆
 
-numOne = int(numStr[0])
+# ####################################
+# #Write your code below this line 👇
 
-numTwo = int(numStr[1])
+# numStr = str(two_digit_number)
 
-sum = numOne + numTwo
-print(sum)
+# numOne = int(numStr[0])
+
+# numTwo = int(numStr[1])
+
+# sum = numOne + numTwo
+# print(sum)
 
 
-## simple conditionals
-number = int(input("what is your height in cm?"))
+# ## simple conditionals
+# number = int(input("what is your height in cm?"))
 
-if number % 2 == 0:
-    print("This is an even number.")
-else :
-    print("This is an odd number.")
+# if number % 2 == 0:
+#     print("This is an even number.")
+# else :
+#     print("This is an odd number.")
 
-##rollercoster ride 
+# ##rollercoster ride 
 
-print("welcome to the rollercoaster")
-height = int(input("what is your height in cm?"))
+# print("welcome to the rollercoaster")
+# height = int(input("what is your height in cm?"))
 
-if height >= 120:
-  print("you can ride the rollercoaster!")
-  age = int(input("What is your age?"))
-  if age < 12:
-    print("Child tickets are $5.")
-    bill = 5
-  elif age <= 18:
-    print("Youth tickets are $7") 
-    bill = 7
-  elif age > 18: 
-    print("Adult tickets are $12")
-    bill = 12
-  elif age >= 45 and age <= 55:
-    bill = 0
-    print("midlife crisis tickets are $0")
-  photos = input("Would you like a photo taken? Type Y for yes and N for no.")
-  if photos == "Y" or photos == "y":
-    bill += 3
-    print(f"Your bill is ${bill}")
-  else:
-    print(f"Your bill is ${bill}")
+# if height >= 120:
+#   print("you can ride the rollercoaster!")
+#   age = int(input("What is your age?"))
+#   if age < 12:
+#     print("Child tickets are $5.")
+#     bill = 5
+#   elif age <= 18:
+#     print("Youth tickets are $7") 
+#     bill = 7
+#   elif age > 18: 
+#     print("Adult tickets are $12")
+#     bill = 12
+#   elif age >= 45 and age <= 55:
+#     bill = 0
+#     print("midlife crisis tickets are $0")
+#   photos = input("Would you like a photo taken? Type Y for yes and N for no.")
+#   if photos == "Y" or photos == "y":
+#     bill += 3
+#     print(f"Your bill is ${bill}")
+#   else:
+#     print(f"Your bill is ${bill}")
+# else:
+#   print("Sorry, you have to grow taller before you can ride")
+
+# random_int = random.randint(1,10)
+
+# random_float = random.random() * 5
+
+random_int = random.randint(0,1)
+
+if random_int == 1:
+    print("Heads")
 else:
-  print("Sorry, you have to grow taller before you can ride")
+    print("Tails")
+
+items = ["can","shovel","spoon","notebook"]
+
