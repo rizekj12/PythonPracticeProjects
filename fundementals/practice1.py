b2de6d476f9eb0cf168a7f477967ad946c733556
Joshua Rizek <joshuarizek@gmail.com>
2023-08-27 20:25:47 -0400

    Update practice1.py

--- fundementals/practice1.py
+++ fundementals/practice1.py
@@ -84,3 +84,34 @@ random_num = random.randint(0, len(names) - 1)
 print(names[random_num])
 
 
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+
+chosenRow = f"row{position[0]}"
+chosenColumn = int(position[1]) - 1
+
+# print(chosenRow[position[1]])
+
+if chosenRow == 'row1':
+    row1[chosenColumn] = "x"
+elif chosenRow == 'row2':
+    row2[chosenColumn] = "x"
+elif chosenRow == 'row3':
+    row3[chosenColumn] = "x"
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
+
+
+
